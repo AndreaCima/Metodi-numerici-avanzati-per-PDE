d221f36eaea089d26ae572bfadc43e781655c76e
AndreaCima <69358424+AndreaCima@users.noreply.github.com>
2026-04-18 14:17:25 +0200

    cambiati parametri

--- Progetto esame/studio_errore.m
+++ Progetto esame/studio_errore.m
@@ -2,8 +2,8 @@ clear; clc; close all;
 % calcolo una soluzione molto accurata con il metodo BEM (N alto) e poi la confronto
 % con soluzioni progressivamente meno accurate per vedere come si comporta
 % l'errore
-N_ref = 200;
-N_val = 160:-20:20;
+N_ref = 150;
+N_val = 90:-10:20;
 n_test = length(N_val);
 
 Err = zeros(n_test, 1);
@@ -18,7 +18,7 @@ for s=1:n_test
     times(s, :) = [time_assembling, time_lin_sist, time_plot];
 end
 
-plot(N_val, Err, LineWidth=2)
+semilogy(N_val, Err, LineWidth=2)
 grid on
 hold on
 title("Error")
