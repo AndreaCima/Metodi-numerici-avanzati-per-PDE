c7fb542fbed8e090f787c1ff6cdb8157bdb8dfcf
AndreaCima <69358424+AndreaCima@users.noreply.github.com>
2026-06-09 15:36:55 +0200

    piccola modifica

--- Progetto esame/test/errore_poligono.m
+++ Progetto esame/test/errore_poligono.m
@@ -3,16 +3,22 @@
 % fine
 clear; clc; close all; 
 
-v = 0.5*[-1+1i, -1-1i, 1-1i];
+v = 0.5* [1+1i, -1+1i, -1-1i, 1-1i];
 N_ref = 2^12;
 N = 2.^(4:11);
 k = 20; 
-theta = pi/4;
+theta = -pi/4;
 
 x_plot = linspace(-1.5, 1.5, 151); % n_points_plot = 150
 [X, Y] = meshgrid(x_plot(1:end-1));
 
-mask = ~inpolygon(X, Y, real(v), imag(v));
+% mask = ~inpolygon(X, Y, real(v), imag(v)); % caso generale 
+
+% toglere commento alle mask sotto per confronto con MPSpack
+mask = linspace(-1.5, 1.5, 151);
+mask = mask(1:end-1);
+[X_mask, Y_mask] = meshgrid(mask);
+mask = ~(X_mask.^2 + Y_mask.^2 <= 1); % complementare del disco unitario
 
 fprintf("Computing u_ref \t")
 [u_ref, ~, ~] = BEM_func(N_ref, k, v, theta);
@@ -32,7 +38,7 @@ for s = 1:length(N)
     Err(s) = norm(u_scat(mask) - u_ref(mask), 2)./norm(u_ref(mask), 2);
 
 end
-p = polyfit(H, Err, 1);
+p = polyfit(log10(H(end-3:end)), log10(Err(end-3:end)), 1);
 slope = p(1);
 clc; 
 fprintf("Slope = %f\n", slope)
@@ -50,7 +56,8 @@ title("Error vs Ndof", Interpreter="latex")
 
 figure; 
 loglog(H, Err, 'bo-', LineWidth=2)
-grid on;
+grid on; hold on;
+
 title("Error vs mesh size", Interpreter="latex")
 
 
