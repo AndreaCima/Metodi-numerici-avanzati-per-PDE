53daa6d4ad0de063275fb52aa11c97a4f08e5324
AndreaCima <69358424+AndreaCima@users.noreply.github.com>
2026-06-28 16:34:19 +0200

    piccola modifica

--- Progetto esame/test/FFP/FFP_approx_circonferenza.m
+++ Progetto esame/test/FFP/FFP_approx_circonferenza.m
@@ -5,15 +5,15 @@ clear; clc; close all;
 % mi aspetto che aumentando il numero di lati ho approssimazione sempre più
 %  accurata
 
-max_edges = 12; % uso tanti gradi di libertà a quanti sono i vertici
-edges = 2.^(4:max_edges);
+max_edges = 12; 
+edges = 2.^(4:max_edges); % uso tanti gradi di libertà a quanti sono i vertici
 
 R = 1; % raggio circonferenza
 z0 = 0 + 0*1i; % centro circonferenza
 k_val = [5, 10, 15, 20]; 
 theta_inc = pi/4; 
-d = exp(1i*theta_inc);
 
+d = exp(1i*theta_inc);
 theta = linspace(0, 2*pi, 360)';
 FFP_ref = cell(length(k_val), 1);
 for k = 1:length(k_val)
@@ -34,7 +34,7 @@ for k = 1:length(k_val)
     L_max = ceil(k*R)+20;
     for n = progress(1:length(edges))
         v = z0 + R * exp( 1i * (0:edges(n)-1) * 2*pi / edges(n) ); % vertici del poligono inscritto
-        [FFP, ~] = Far_Field(5*edges(n), k, v, theta_inc);
+        [FFP, ~] = Far_Field(edges(n), k, v, theta_inc);
         Err(n, k) = norm(FFP-FFP_ref{k}, 2)/norm(FFP_ref{k}, 2);
     end
 
