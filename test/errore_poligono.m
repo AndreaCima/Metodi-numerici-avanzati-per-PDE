e8dda714f57cc034340082b7f5996b4f971b765d
AndreaCima <69358424+AndreaCima@users.noreply.github.com>
2026-07-13 18:23:50 +0200

    sistemazione grafici

--- test/errore_poligono.m
+++ test/errore_poligono.m
@@ -12,13 +12,13 @@ theta = -pi/4;
 x_plot = linspace(-1.5, 1.5, 151); % n_points_plot = 150
 [X, Y] = meshgrid(x_plot(1:end-1));
 
-% mask = ~inpolygon(X, Y, real(v), imag(v)); % caso generale 
+mask = ~inpolygon(X, Y, real(v), imag(v)); % caso generale 
 
 % toglere commento alle mask sotto per confronto con MPSpack
-mask = linspace(-1.5, 1.5, 151);
-mask = mask(1:end-1);
-[X_mask, Y_mask] = meshgrid(mask);
-mask = ~(X_mask.^2 + Y_mask.^2 <= 1); % complementare del disco unitario
+% mask = linspace(-1.5, 1.5, 151);
+% mask = mask(1:end-1);
+% [X_mask, Y_mask] = meshgrid(mask);
+% mask = ~(X_mask.^2 + Y_mask.^2 <= 1); % complementare del disco unitario
 
 fprintf("Computing u_ref \t")
 [u_ref, ~, ~] = BEM_func(N_ref, k, v, theta);
@@ -48,18 +48,20 @@ figure;
 loglog(N, times, 'bo-', LineWidth=2)
 grid on; 
 title("Plotting time vs Ndof", Interpreter="latex")
+xlabel("degrees of freedom")
+ylabel('time (s)')
 
 figure; 
 loglog(N, Err, 'bo-', LineWidth=2)
 grid on;
 title("Error vs Ndof", Interpreter="latex")
+xlabel("degrees of freedom")
 
 figure; 
 loglog(H, Err, 'bo-', LineWidth=2)
 grid on; hold on;
-
 title("Error vs mesh size", Interpreter="latex")
-
+xlabel("mesh size")
 
 
 
